687cd7c5a52af5ec6b7d74a1169d9b0aa3ceded4
Wei Kang <chongweikang5@gmail.com>
2023-07-27 22:23:00 +0800

    Implement home screen, header, footer, routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -9,6 +10,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to CommerceBay</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
