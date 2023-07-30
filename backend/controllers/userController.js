67744ee1e77206f45442f2d21fda37e0d221a084
Wei Kang <chongweikang5@gmail.com>
2023-07-30 23:36:48 +0800

    Implement log out clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       },
 
       process.env.JWT_SECRET,
-      
+
       {
         expiresIn: "30d",
       }
@@ -56,7 +56,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc Get user profile
