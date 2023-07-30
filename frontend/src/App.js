6ace7d37ade9d881d17e59d57318b14b191483da
Wei Kang <chongweikang5@gmail.com>
2023-07-31 00:05:19 +0800

    Set up auth slice and userApi slice and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to CommerceBay</h1>
           <Outlet />
         </Container>
       </main>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import store from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path= "/login" element={<LoginScreen />} />
     </Route>
   )
 );
