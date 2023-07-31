8aeec41d28e686267862dcb851eae7a372c7ca4b
Wei Kang <chongweikang5@gmail.com>
2023-07-31 11:44:21 +0800

    Create Shipping Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path= "/login" element={<LoginScreen />} />
       <Route path= "/register" element={<RegisterScreen />} />
+      <Route path= "/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,8 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
-
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -20,17 +19,19 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
       return updateCart(state);
-
     },
     removeFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter(
-        (x) => x._id !== action.payload
-      );
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
-    }
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
