c2f9240ac0ca4401be788e41428622a87dff6f3e
Wei Kang <chongweikang5@gmail.com>
2023-08-07 13:21:16 +0800

    Create ProductListScreen for Admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderListScreen from "./screens/admin/OrderListScreen";
+import ProductListScreen from "./screens/admin/ProductListScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import AdminRoute from "./components/AdminRoute";
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -46,6 +47,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen />} />
       </Route>
     </Route>
   )
